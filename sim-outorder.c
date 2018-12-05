1240f1d80332d1a354be93b9d5eed0246f516fb3
Ken Koch <kek33@duke.edu>
2018-12-04 22:45:57 -0500

    More prints

--- sim-outorder.c
+++ sim-outorder.c
@@ -4571,7 +4571,7 @@ ruu_fetch(void)
             }
           }
         }
-        fprintf(stderr, "Is thread 0 in use?: %d\n", thread_states[0].in_use
+        fprintf(stderr, "Is thread 0 in use?: %d\n", thread_states[0].in_use);
         fprintf(stderr, "current fetching thread: %d\n", current_fetching_thread);
       }
 
