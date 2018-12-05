fbe89f691c92760d3e6d84b9a8e14a0f85e2c357
Ken Koch <kek33@duke.edu>
2018-12-05 15:36:32 -0500

    Bring back

--- sim-outorder.c
+++ sim-outorder.c
@@ -2268,7 +2268,7 @@ ruu_commit(void)
       }
       if (rs->spec_mode) {
         fprintf(stderr, "Speculative insn committing on thread: %d\n", rs->thread_id);
-        //panic("speculative insns should not be committing");
+        panic("speculative insns should not be committing");
       };
 
       // The forked thread off this is the correct one, so this can retire now
@@ -2453,8 +2453,8 @@ ruu_commit(void)
        } */
        if (RUU[RUU_index].spec_mode == FALSE) {
          fprintf(stderr, "Bad: triggers fork (%d)\n", RUU[branch_index].triggers_fork);
-         //panic("Trying to squash correct instructions. Incorrectly squashed thread (%d) has fork counter (%d) and is squashed by thread (%d) with counter (%d)",
-          //RUU[RUU_index].thread_id, thread_states[RUU[RUU_index].thread_id].parent_fork_counters[thread_id], thread_id, fork_counter);
+         panic("Trying to squash correct instructions. Incorrectly squashed thread (%d) has fork counter (%d) and is squashed by thread (%d) with counter (%d)",
+          RUU[RUU_index].thread_id, thread_states[RUU[RUU_index].thread_id].parent_fork_counters[thread_id], thread_id, fork_counter);
        }
 
        /* is this operation an effective addr calc for a load or store? */
